9d04c8d4fbb4ad04c25b21ec49e458198115d5b2
Fizza-Rubab <fr06161@st.habib.edu.pk>
2022-01-08 22:32:06 +0500

    minor changes

--- api/gradeAssigment.py
+++ api/gradeAssigment.py
@@ -1,11 +1,10 @@
 from canvas_lms_api import Canvas
 grader = Canvas(base="https://hulms.instructure.com", token="17361~ygCxdvuMbLXDtgiCpEdqLrRRZ02kASevZHl9vddtJPtBDS7MGs8WvT5wlPcapUcB", course="1923")
 # Find your assignment id number see Get Assignments Example
-assignment_id = "19644"
-# Find your student id number see Get Course Users Example
-student_id = ""
+assignment_id = "19794"
+student_id = "1048"
 
 # Set score and comment
-score = "100.35"
-comment = "The student failed to complete the assignment\nAnd they got thse points wrong\n(-10) for poor guessing"
+score = "0.5"
+comment = "Grace marks"
 grader.SubmitGrade(assignment_id, student_id, score, comment, visibility=False)
