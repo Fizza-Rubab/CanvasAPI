81c6b62bcbce101a92e593f3224dd37de2c44401
Fizza-Rubab <fr06161@st.habib.edu.pk>
2022-01-17 16:42:20 +0500

    syllabus updated

--- coursefiles/syllabus.py
+++ coursefiles/syllabus.py
@@ -3,8 +3,11 @@ from config import *
 courseID = 1923
 course = canvas.get_course(courseID)
 f  = (course.get_files())
+
+# Iterate over course files
 for i in f:
-    if str(i)=="HW 1 Scores.pdf":
-        i.download("./canvasapi/"+str(i))
+    # Find the one titled syllabus.pdf
+    if str(i)=="syllabus.pdf":
+        i.download("./"+str(i))
         print("Syllabus downloaded")
         break
