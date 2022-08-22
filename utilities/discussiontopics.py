5c7f1517725c13486c7c7812734a4ed2441ce3a1
Fizza Rubab <59685719+Fizza-Rubab@users.noreply.github.com>
2022-08-22 22:44:53 +0500

    updated

---
+++ utilities/discussiontopics.py
@@ -0,0 +1,25 @@
+from time import sleep
+from canvasapi import *
+
+API_URL ="https://hulms.instructure.com"
+API_KEY = "17361~0yOJPT02iOLJ5ZUoVvSRhYvdf7AAJ4kv4ET95osteKVuf8Jxbynvq0bgFGn37UST"
+canvas = Canvas(API_URL, API_KEY)
+
+# Set the course
+course_id = 2008
+course = canvas.get_course(course_id)
+total_weeks = 14
+
+for i in range(1, total_weeks+1):
+    print("Creating Discussion for week %02d"%i)
+    title = "Week %02d Discussion"%i
+    
+    # Write the discussion description
+    message = "Let us discuss the topics covered in Week %02d."%i
+
+    # Create the published discussion
+    course.create_discussion_topic(title=title, message=message, published=True, # Published can be set to false to avoid notifications
+    discussion_type="threaded")
+    
+    # Delay added
+    sleep(2)
