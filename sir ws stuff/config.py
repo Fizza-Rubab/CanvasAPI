4e4b0c6d70f939bf4c6749ef094440011c25860e
Fizza Rubab <59685719+Fizza-Rubab@users.noreply.github.com>
2022-08-22 22:46:46 +0500

    deleted and restructured

--- sir ws stuff/config.py
+++
@@ -1,7 +0,0 @@
-from canvasapi import *
-from datetime import datetime
-from canvasapi import section
-# Setting the parameters
-API_URL ="https://hulms.instructure.com"
-API_KEY = "17361~ygCxdvuMbLXDtgiCpEdqLrRRZ02kASevZHl9vddtJPtBDS7MGs8WvT5wlPcapUcB"
-canvas = Canvas(API_URL, API_KEY)
